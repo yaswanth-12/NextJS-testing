e19beb9ca1975855dfdf89d343f63907d1dc954f
Yaswanth Gadamsetti <yaswanthn70@gmail.com>
2023-04-13 14:28:23 +0530

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,15 +2,10 @@ import Navbar from '../components/Navbar'
 import Main from '../components/Main'
 import styles from '../styles/main.module.css'
 import Cards from '../components/Cards'
-import {Head} from 'next/document'
 
 export default function Home() {
   return (
     <>
-      <Head>
-          <link rel="icon" href="https://yaswanth-12.github.io/NextJS-testing/favicon.ico" />
-          <meta name="viewport" content="width=device-width, initial-scale=1" />
-      </Head>
       <main>
         <Navbar />
         <Main />
